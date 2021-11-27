9396aefdb226da256c95413bceeaa9e8cb479d5f
markadriii <69063424+markadriii@users.noreply.github.com>
2021-11-27 21:36:50 +0800

    Updated EJS files

--- models/account.js
+++ models/account.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose')
 mongoose.pluralize(null)
 
 const accountSchema = new mongoose.Schema({
-    FirstName: String,
-    LastName: String,
     Username: String,
     Email: String,
     Password: String
