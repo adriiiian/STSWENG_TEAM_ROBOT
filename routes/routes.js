9624472a220c96d0061c2d2da42c9968fcbc2430
markadriii <69063424+markadriii@users.noreply.github.com>
2021-11-27 10:01:06 +0800

    Changed HTML files to EJS

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,19 @@ const loginController = require('../controllers/login-controller');
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('test')
+    res.render('index')
 })
 
+router.get('/index', (req, res) => {
+    res.redirect('/')
+})
+
+
+router.get('/services', (req, res) => {
+    res.render('services')
+})
+
+router.get('/view_services', (req, res) => {
+    res.render('view_services')
+})
 module.exports = router
