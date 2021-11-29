0fcc40f872a50d572a565a3688874fa7a985ff0e
Ina Evangelista <ina_evangelista@dlsu.edu.ph>
2021-11-29 22:04:39 +0800

    US4 (frontend)

--- routes/routes.js
+++ routes/routes.js
@@ -12,7 +12,6 @@ router.get('/index', (req, res) => {
     res.redirect('/')
 })
 
-
 router.get('/services', (req, res) => {
     res.render('services')
 })
@@ -23,4 +22,11 @@ router.get('/view_services', (req, res) => {
 
 router.get('/check-login-info', loginController.Login)
 
+router.get('/rooms', (req, res) => {
+    res.render('rooms')
+})
+
+router.get('/view_rooms', (req, res) => {
+    res.render('view_rooms')
+})
 module.exports = router
