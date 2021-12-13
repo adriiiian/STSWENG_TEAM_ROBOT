ae4ad1ccdaf4549c5caddd4febe35041885d94f9
markadriii <69063424+markadriii@users.noreply.github.com>
2021-12-13 19:45:21 +0800

    Added unit tests for login feature

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,6 +1,8 @@
 const db = require('../models/database.js')
-
+const helper = require('../helpers/login-check.js')
 var _email = ''
+
+ 
 
 const loginController = { 
     viewIndex: (req, res) => {
@@ -9,34 +11,38 @@ const loginController = {
         }
         res.render('index', {_email})
     },
-
+    
     Login: async (req, res) => {
         var LoginInfo = req.query.LoginInfo
-        
-        await db.Account.findOne({Email: LoginInfo.Email})
-            .then((User) => {
-                
-                if(User) {
-                    if(User.Password == LoginInfo.Password) {
+        var result = await helper.CheckLoginError(LoginInfo.Email, LoginInfo.Password)
+        if(result == 'login information is valid') {
+            await db.Account.findOne({Email: LoginInfo.Email})
+                .then((User) => {
+                    
+                    if(User) {
+                        if(User.Password == LoginInfo.Password) {
 
-                        if(LoginInfo.RememberMe == 'true'){
-                            req.session.email = User.Email
-                            req.session.username = User.Username
+                            if(LoginInfo.RememberMe == 'true'){
+                                req.session.email = User.Email
+                                req.session.username = User.Username
+                            }
+                                
+                            res.send('success')
                         }
-                            
-                        res.send('success')
+                        else {
+                            res.send('invalid-pw')
+                        }
                     }
                     else {
-                        res.send('invalid-pw')
+                        res.send('invalid-email')
                     }
-                }
-                else {
-                    res.send('invalid-email')
-                }
 
-            })
+                })
+        }
+        
+        
     },
-    
+
     Save: (req, res) => {
         _email = req.query.Email
         res.send('success')
