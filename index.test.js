960d67ae72a6d08d3ee6ea8fc851a7ab395a3788
Adrian Jerez <69713328+adriiiian@users.noreply.github.com>
2021-12-14 14:47:40 +0800

    added unit tests

--- index.test.js
+++ index.test.js
@@ -1,13 +1,58 @@
-const { Register } = require('./controllers/register-controller');
+const { Register, CheckFields } = require('./controllers/register-controller');
 
-describe('All input fields are valid and email is not yet used', () => {
-    it('When all input fields are valid and email is not yet used', () => {
-        const Email = "Test01@gmail.com"
-        const Username = "Test01"
-        const Password = "Password"
+describe('All input fields is valid', () => {
+    it('When All input fields is valid', () => {
+        var RegInfo = {
+            Email: "Test01@gmail.com",
+            Username = "Test01",
+            Password = "Password"
+        }
 
-        const result = Register(Email, Username, Password);
+        const result = CheckFields(RegInfo);
 
-        expect(result).toEqual("Successfully registered")
+        expect(result).toEqual("valid")
+    })
+})
+
+describe('Username field is empty', () => {
+    it('When Username field is empty', () => {
+        var RegInfo = {
+            Email: "Test01@gmail.com",
+            Username = "",
+            Password = "Password"
+        }
+
+        const result = CheckFields(RegInfo);
+
+        expect(result).toEqual("invalid username")
+    })
+})
+
+describe('Email field is empty', () => {
+    it('When Email field is empty', () => {
+        var RegInfo = {
+            Email: "",
+            Username = "Test01",
+            Password = "Password"
+        }
+
+        const result = CheckFields(RegInfo);
+
+        expect(result).toEqual("invalid email")
+    })
+})
+
+
+describe('Password field is empty', () => {
+    it('When Password field is empty', () => {
+        var RegInfo = {
+            Email: "Test01@gmail.com",
+            Username = "Test01",
+            Password = ""
+        }
+
+        const result = CheckFields(RegInfo);
+
+        expect(result).toEqual("invalid password")
     })
 })
