d2d1cdd3d584858596bbf75d3b1759e4c2b1f8ec
Adrian Jerez <69713328+adriiiian@users.noreply.github.com>
2022-01-26 00:43:24 +0800

    Added price change when room selection changed
    Also added model for booking

---
+++ models/bookings.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+mongoose.pluralize(null)
+
+const bookingsSchema = new mongoose.Schema({
+    Fullname: String,
+    Email: String,
+    ContactNumber: String,
+    Address: String,
+    Checkin: Date,
+    Checkout: Date,
+    RoomType: String,
+    Guests: Number
+})
+
+const Bookings = mongoose.model('Bookings', bookingsSchema)
+module.exports = Bookings
