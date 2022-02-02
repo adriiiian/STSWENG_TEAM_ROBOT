49ffcb738ee82b36f06b2b803f256404a6c8e8e5
Adrian Jerez <69713328+adriiiian@users.noreply.github.com>
2022-02-02 18:38:18 +0800

    added readonly for datepickers

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -6,10 +6,7 @@ const adminController = {
         var tempDate = new Date();
         var rooms;
         var currDate = (tempDate.getMonth() + 1) + "-" + tempDate.getDate() + "-" + tempDate.getFullYear()
-        console.log('Curr date ' + currDate);
         await db.Bookings.find({Checkin: currDate, RoomType: "Single Room"}).then((bookings) => {
-            console.log('hello');
-            console.log(bookings);
             rooms = {
                 room1: "Not occupied",
                 room2: "Not occupied",
@@ -18,7 +15,6 @@ const adminController = {
                 room5: "Not occupied"
             }
             for(let i = 0; i < bookings.length; i++){
-                // console.log(bookings[i])
                 if(bookings[i].RoomNumber == 101){
                     rooms.room1 = "Occupied"
                 }
@@ -35,26 +31,8 @@ const adminController = {
                     rooms.room5 = "Occupied"
                 }
             }
-            // res.send(rooms);
-            res.render('admin', {rooms});
-            
-            // res.render('admin', {rooms});
-            // if(rooms){
-            //     res.render('admin', {rooms});
-            // }
+            res.render('admin', {rooms, currDate});
         });
-        // res.send(rooms)
-        // if(req.session.email) {
-        //     _email = req.session.email
-
-        //     var id = req.query.id
-
-        //     await db.Bookings.findOne({_id: id}).then(function(booking) {
-        //         if(booking) {
-        //             res.render('view_transactions', {_email, booking})
-        //         }
-        //     })
-        // }
     },
 }
 
