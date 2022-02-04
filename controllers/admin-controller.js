bb62cc8e4695ebadd0fa108c0bf1bfe587294d8b
Adrian Jerez <69713328+adriiiian@users.noreply.github.com>
2022-02-05 00:59:58 +0800

    added occupant column for admin page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,10 +1,9 @@
 const db = require('../models/database.js')
-const { selectFields } = require('express-validator/src/select-fields')
 
 const adminController = { 
     viewOccupiedRooms: async (req, res) => {
         var tempDate = new Date();
-        var rooms;
+        var rooms, names;
         var currDate = (tempDate.getMonth() + 1) + "-" + tempDate.getDate() + "-" + tempDate.getFullYear()
 
         if(req.session.adminEmail) {
@@ -16,22 +15,35 @@ const adminController = {
                     room4: "Not occupied",
                     room5: "Not occupied"
                 }
+                names = {
+                    name1: "None",
+                    name2: "None",
+                    name3: "None",
+                    name4: "None",
+                    name5: "None"
+                }
+    
                 for(let i = 0; i < bookings.length; i++){
                     if(tempDate >= (new Date(bookings[i].Checkin)) && tempDate < (new Date(bookings[i].Checkout))){
                         if(bookings[i].RoomNumber == 101){
                             rooms.room1 = "Occupied"
+                            names.name1 = bookings[i].Fullname
                         }
                         else if(bookings[i].RoomNumber == 102){
                             rooms.room2 = "Occupied"
+                            names.name2 = bookings[i].Fullname
                         }
                         else if(bookings[i].RoomNumber == 103){
                             rooms.room3 = "Occupied"
+                            names.name3 = bookings[i].Fullname
                         }
                         else if(bookings[i].RoomNumber == 104){
                             rooms.room4 = "Occupied"
+                            names.name4 = bookings[i].Fullname
                         }
                         else if(bookings[i].RoomNumber == 105){
                             rooms.room5 = "Occupied"
+                            names.name5 = bookings[i].Fullname
                         }
                     }
                 }
@@ -44,7 +56,7 @@ const adminController = {
                 else if(tempDate.getMonth() < 9){
                     currDate = "0" + (tempDate.getMonth() + 1) + "-" + tempDate.getDate() + "-" + tempDate.getFullYear();
                 }
-                res.render('admin', {rooms, currDate});
+                res.render('admin', {rooms, names, currDate});
             });
         }
         else {
@@ -56,9 +68,8 @@ const adminController = {
     viewViewOccupiedRooms: async (req, res) => {
         var tempDate = new Date(req.query.selectedDate);
         var roomType = req.query.roomType;
-        var rooms;
+        var rooms, names;
         var roomNum = parseInt(req.query.multiplier);
-        var currDate = (tempDate.getMonth() + 1) + "-" + tempDate.getDate() + "-" + tempDate.getFullYear()
         await db.Bookings.find({RoomType: roomType}).then((bookings) => {
             rooms = {
                 room1: "Not occupied",
@@ -67,26 +78,40 @@ const adminController = {
                 room4: "Not occupied",
                 room5: "Not occupied"
             }
+
+            names = {
+                name1: "None",
+                name2: "None",
+                name3: "None",
+                name4: "None",
+                name5: "None"
+            }
+
             for(let i = 0; i < bookings.length; i++){
                 if(tempDate >= (new Date(bookings[i].Checkin)) && tempDate < (new Date(bookings[i].Checkout))){
                     if(bookings[i].RoomNumber == (roomNum + 1)){
                         rooms.room1 = "Occupied"
+                        names.name1 = bookings[i].Fullname
                     }
                     else if(bookings[i].RoomNumber == (roomNum + 2)){
                         rooms.room2 = "Occupied"
+                        names.name2 = bookings[i].Fullname
                     }
                     else if(bookings[i].RoomNumber == (roomNum + 3)){
                         rooms.room3 = "Occupied"
+                        names.name3 = bookings[i].Fullname
                     }
                     else if(bookings[i].RoomNumber == (roomNum + 4)){
                         rooms.room4 = "Occupied"
+                        names.name4 = bookings[i].Fullname
                     }
                     else if(bookings[i].RoomNumber == (roomNum + 5)){
                         rooms.room5 = "Occupied"
+                        names.name5 = bookings[i].Fullname
                     }
                 }
             }
-            res.send(rooms)
+            res.send({rooms: rooms, names: names})
         });
     },
 }
