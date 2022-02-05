42e3f0af031994b4cc7027f9089167eccc69fe88
markadriii <69063424+markadriii@users.noreply.github.com>
2022-02-05 23:29:23 +0800

    Implemented discount tab

---
+++ public/js/admin.js
@@ -0,0 +1,48 @@
+$(document).ready(function () 
+{
+    const roomprice = [15000, 20000, 25000, 35000, 40000, 45000];
+    const roomtype = ['Single', 'Double', 'Triple', 'Quad', 'King', 'Queen'];
+
+    $("#discount_room").change(function() {
+        $('#discount').prop('disabled', true)
+        $.get('get-room-price-discount', {RoomType: $('#discount_room :selected').val()},function(result) {
+
+            let currPrice = result.Price
+            let currRoom = result.Type
+
+            for(var i = 0; i < roomtype.length; i++) {
+                if(currRoom == roomtype[i]) {
+                    let discount = ((roomprice[i] - currPrice) / roomprice[i]) * 100
+
+                    $('#discount').val(discount)
+                    $('#discount').prop('disabled', false)
+                }
+            }
+        })
+    });
+
+
+    $('#btn_discount').click(function() {
+        
+        let input = $('#discount').val()
+
+        if(input >= 0 && input <= 100) {
+            $.get('get-room-price-discount', {RoomType: $('#discount_room :selected').val()},function(result) {
+
+                let currRoom = result.Type
+    
+                for(var i = 0; i < roomtype.length; i++) {
+                    if(currRoom == roomtype[i]) {
+                        let discountedPrice = roomprice[i] - ((input / 100) * roomprice[i])
+    
+                        $.post('update-room-price', {Price: discountedPrice, Type: currRoom}, function(result) {
+                            console.log('result is -> ' + result)
+                        })
+                    }
+                }
+            })
+        }
+        
+        
+    })
+})
